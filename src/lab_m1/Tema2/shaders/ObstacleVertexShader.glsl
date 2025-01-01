0128523b2ea82afeab8a7221cc82f43bc346b655
dandominicstaicu <dando.ds11@gmail.com>
2025-01-01 21:38:50 +0200

    Tema2: reverted to simple shaders for obstacles

--- src/lab_m1/Tema2/shaders/ObstacleVertexShader.glsl
+++ src/lab_m1/Tema2/shaders/ObstacleVertexShader.glsl
@@ -1,18 +1,13 @@
 #version 330 core
 
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec3 color; // Can be ignored or used for additional features
+layout(location = 1) in vec3 normal; // Can be ignored if not used
+layout(location = 2) in vec3 color;  // Can be ignored or used for additional features
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 FragPos; // For lighting (optional)
-out vec3 Normal;  // For lighting (optional)
-
 void main() {
-    FragPos = vec3(model * vec4(position, 1.0));
-    Normal = mat3(transpose(inverse(model))) * normal;  
     gl_Position = projection * view * model * vec4(position, 1.0);
 }

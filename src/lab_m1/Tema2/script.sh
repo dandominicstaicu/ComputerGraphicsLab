3ca594a079a386019cf19976a2650c61655c76ac
dandominicstaicu <dando.ds11@gmail.com>
2025-01-07 22:50:20 +0200

    last

--- src/lab_m1/Tema2/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Count lines in all files in the current directory and subdirectories
-total_lines=$(find . -type f -exec wc -l {} + | awk '{total += $1} END {print total}')
-
-# Print the result
-echo "Total lines in all files: $total_lines"
-
